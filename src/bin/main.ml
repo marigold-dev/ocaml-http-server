8396902eddd3f95427f5ca5a04972fb33dffbf44
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-11-07 10:31:51 +0100

    feat: two endpoints
    - One that does nothing: "/route-a"
    - One that does deserialization/serialization of the request/response:
      "/route-b"

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,6 +1,58 @@
-let _route = Routes.(s "echo" /? nil)
 
-let request_handler _ = Piaf.Response.create `OK
+(* Empty route *)
+let route_a _ = Piaf.Response.create `OK
+
+
+(* with deserialization, and a non empty request/response *)
+module Request = struct
+  type operation = Operation_noop of {
+    sender: string
+  } [@@deriving of_yojson]
+
+  type initial = 
+    | Initial_operation of {
+      hash: string;
+      nonce: string;
+      level: string;
+      operation: operation
+  } [@@deriving of_yojson]
+  
+  type t = {
+    key: string;
+    signature: string;
+    initial: initial
+  } [@@deriving of_yojson]
+end
+
+module Response = struct
+  type t = {hash: string} [@@deriving yojson_of]
+
+end
+
+let route_b req =  
+  let Piaf.Server.Handler.{ request : Piaf.Request.t; _ } = req in
+  let body = request.body
+    |> Piaf.Body.to_string
+    |> Result.get_ok
+    |> Yojson.Safe.from_string
+    |> Request.t_of_yojson
+  in
+  let Request.{initial; key=_; signature=_} = body in  
+  let Request.Initial_operation {hash; _} = initial in
+  let response = {hash = hash}
+    |> Response.yojson_of_t
+    |> Yojson.Safe.to_string
+    |> Piaf.Body.of_string
+  in
+  Piaf.Response.create ~body:response `OK
+
+let request_handler req = 
+  let Piaf.Server.Handler.{ request : Piaf.Request.t; _ } = req in
+  let target = request.target in
+  match target with
+    | "/route-a" -> route_a req
+    | "/route-b" -> route_b req
+    | _ -> Piaf.Response.create `Not_found
 
 let () =
   Eio_main.run @@ fun env ->
